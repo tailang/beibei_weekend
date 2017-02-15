f39cec3989d54dc734c813edb4e221ca45f714ee
😈caoping <tailangdev@gmail.com>
2017-02-15 10:59:56 +0800

    Fixed:变量名修改

--- lib/beibei_weekend/workspace.rb
+++ lib/beibei_weekend/workspace.rb
@@ -11,8 +11,8 @@ module BeibeiWeekend
           Log.info "正在创建workspace..."
           begin
             Dir.mkdir workspace_name
-            @resource_dir = File.expand_path('../resource', __FILE__)
-            FileUtils.cp_r @resource_dir, "./#{workspace_name}/"
+            resource_dir = File.expand_path('../resource', __FILE__)
+            FileUtils.cp_r resource_dir, "./#{workspace_name}/"
           rescue
             Log.error "创建workspace失败,请重新尝试!"
           end
